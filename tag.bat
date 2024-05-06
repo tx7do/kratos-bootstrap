8044bb92b3baf760831ef82854785487e210cde6
tx7do <yanglinbo@gmail.com>
2024-05-06 12:51:50 +0800

    feat: refactor.

--- tag.bat
+++ tag.bat
@@ -7,7 +7,7 @@ git tag cache/redis/v0.0.1 --force
 git tag oss/minio/v0.0.1 --force
 
 git tag config/apollo/v0.0.1 --force
-git tag config/consul/v0.0.1 --force
+git tag config/consul/v0.0.2 --force
 git tag config/etcd/v0.0.1 --force
 git tag config/kubernetes/v0.0.1 --force
 git tag config/nacos/v0.0.1 --force
