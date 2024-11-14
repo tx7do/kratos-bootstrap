0e4d1aa6cb4c6c915b1e710986754d18ffde10b0
tx7do <yanglinbo@gmail.com>
2024-11-14 11:35:26 +0800

    feat: add database.

---
+++ database/ent/client.go
@@ -0,0 +1,37 @@
+package ent
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
+	_ "github.com/lib/pq"
+
+	"github.com/go-kratos/kratos/v2/log"
+
+	conf "github.com/tx7do/kratos-bootstrap/api/gen/go/conf/v1"
+)
+
+// NewEntClient 创建Ent ORM数据库客户端
+func NewEntClient[T ClientInterface](cfg *conf.Bootstrap, l *log.Helper, db T) *ClientWrapper[T] {
+
+	drv, err := CreateDriver(
+		cfg.Data.Database.GetDriver(),
+		cfg.Data.Database.GetSource(),
+		cfg.Data.Database.GetEnableTrace(),
+		cfg.Data.Database.GetEnableMetrics(),
+	)
+	if err != nil {
+		l.Fatalf("failed opening connection to db: %v", err)
+		return nil
+	}
+
+	wrapperClient := NewEntClientWrapper(db, drv)
+
+	wrapperClient.SetConnectionOption(
+		int(cfg.Data.Database.GetMaxIdleConnections()),
+		int(cfg.Data.Database.GetMaxOpenConnections()),
+		cfg.Data.Database.GetConnectionMaxLifetime().AsDuration(),
+	)
+
+	return wrapperClient
+}
---
+++ database/gorm/client.go
@@ -0,0 +1,57 @@
+package gorm
+
+import (
+	"gorm.io/driver/clickhouse"
+	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
+	"gorm.io/driver/sqlite"
+	"gorm.io/driver/sqlserver"
+
+	"gorm.io/gorm"
+
+	"github.com/go-kratos/kratos/v2/log"
+
+	conf "github.com/tx7do/kratos-bootstrap/api/gen/go/conf/v1"
+)
+
+// NewGormClient 创建GORM数据库客户端
+func NewGormClient(cfg *conf.Bootstrap, l *log.Helper, migrates []interface{}) *gorm.DB {
+
+	var driver gorm.Dialector
+	switch cfg.Data.Database.Driver {
+	default:
+		fallthrough
+	case "mysql":
+		driver = mysql.Open(cfg.Data.Database.Source)
+		break
+	case "postgres":
+		driver = postgres.Open(cfg.Data.Database.Source)
+		break
+	case "clickhouse":
+		driver = clickhouse.Open(cfg.Data.Database.Source)
+		break
+	case "sqlite":
+		driver = sqlite.Open(cfg.Data.Database.Source)
+		break
+	case "sqlserver":
+		driver = sqlserver.Open(cfg.Data.Database.Source)
+		break
+	}
+
+	client, err := gorm.Open(driver, &gorm.Config{})
+	if err != nil {
+		l.Fatalf("failed opening connection to db: %v", err)
+		return nil
+	}
+
+	// 运行数据库迁移工具
+	if cfg.Data.Database.Migrate {
+		if err = client.AutoMigrate(
+			migrates...,
+		); err != nil {
+			l.Fatalf("failed creating schema resources: %v", err)
+			return nil
+		}
+	}
+	return client
+}
