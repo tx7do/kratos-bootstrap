0759f2d7521a0496abf93233e4c44110b0865942
Bobo <yanglinbo@gmail.com>
2025-06-02 11:58:11 +0800

    feat: registry.

--- registry/nacos/client_creator.go
+++ registry/nacos/client_creator.go
@@ -21,15 +21,30 @@ func NewRegistry(c *conf.Registry) *Registry {
 	}
 
 	cliConf := nacosConstant.ClientConfig{
-		NamespaceId:          c.Nacos.NamespaceId,
-		TimeoutMs:            uint64(c.Nacos.Timeout.AsDuration().Milliseconds()), // http请求超时时间，单位毫秒
-		BeatInterval:         c.Nacos.BeatInterval.AsDuration().Milliseconds(),    // 心跳间隔时间，单位毫秒
-		UpdateThreadNum:      int(c.Nacos.UpdateThreadNum),                        // 更新服务的线程数
+		NamespaceId: c.Nacos.NamespaceId,
+		RegionId:    c.Nacos.RegionId, // 地域ID
+		AppName:     c.Nacos.AppName,
+		AppKey:      c.Nacos.AppKey,
+
+		TimeoutMs:    uint64(c.Nacos.Timeout.AsDuration().Milliseconds()), // http请求超时时间，单位毫秒
+		BeatInterval: c.Nacos.BeatInterval.AsDuration().Milliseconds(),    // 心跳间隔时间，单位毫秒
+
+		UpdateThreadNum:      int(c.Nacos.UpdateThreadNum), // 更新服务的线程数
 		LogLevel:             c.Nacos.LogLevel,
 		CacheDir:             c.Nacos.CacheDir,             // 缓存目录
 		LogDir:               c.Nacos.LogDir,               // 日志目录
 		NotLoadCacheAtStart:  c.Nacos.NotLoadCacheAtStart,  // 在启动时不读取本地缓存数据，true--不读取，false--读取
 		UpdateCacheWhenEmpty: c.Nacos.UpdateCacheWhenEmpty, // 当服务列表为空时是否更新本地缓存，true--更新,false--不更新
+
+		Username: c.Nacos.Username,
+		Password: c.Nacos.Password,
+
+		OpenKMS: c.Nacos.OpenKms, // 是否开启KMS加密
+
+		AccessKey: c.Nacos.AccessKey, // 阿里云AccessKey
+		SecretKey: c.Nacos.SecretKey, // 阿里云SecretKey
+
+		ContextPath: c.Nacos.ContextPath,
 	}
 
 	cli, err := nacosClients.NewNamingClient(
