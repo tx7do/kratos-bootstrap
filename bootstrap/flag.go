e4b9ed47bb5cb4e936340f3fa4e544b2b477e283
Bobo <yanglinbo@gmail.com>
2025-12-20 14:11:44 +0800

    feat: bootstrap.

--- bootstrap/flag.go
+++ bootstrap/flag.go
@@ -33,8 +33,8 @@ func NewCommandFlags() *CommandFlags {
 func (f *CommandFlags) AddFlags(cmd *cobra.Command) {
 	cmd.PersistentFlags().StringVarP(&f.Conf, "conf", "c", f.Conf, "config path, eg: -conf ../../configs")
 	cmd.PersistentFlags().StringVarP(&f.Env, "env", "e", f.Env, "runtime environment, eg: -env dev")
-	cmd.PersistentFlags().StringVarP(&f.ConfigHost, "chost", "ch", f.ConfigHost, "config server host, eg: -chost 127.0.0.1:8500")
-	cmd.PersistentFlags().StringVarP(&f.ConfigType, "ctype", "ct", f.ConfigType, "config server type, eg: -ctype consul")
+	cmd.PersistentFlags().StringVarP(&f.ConfigHost, "chost", "s", f.ConfigHost, "config server host, eg: -chost 127.0.0.1:8500")
+	cmd.PersistentFlags().StringVarP(&f.ConfigType, "ctype", "t", f.ConfigType, "config server type, eg: -ctype consul")
 	cmd.PersistentFlags().BoolVarP(&f.Daemon, "daemon", "d", f.Daemon, "run app as a daemon with -d or --daemon")
 }
 
