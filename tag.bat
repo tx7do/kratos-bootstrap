49fc8868c4860a35460701a3e73d8d11a4e09819
Bobo <yanglinbo@gmail.com>
2025-12-23 17:01:37 +0800

    feat: transport: asynq server, sse server, kafka server, mqtt server.

--- tag.bat
+++ tag.bat
@@ -42,8 +42,9 @@ git tag config/polaris/v0.2.2 --force
 
 git tag transport/asynq/v0.0.2 --force
 git tag transport/kafka/v0.0.2 --force
-git tag transport/sse/v0.0.2 --force
+git tag transport/mcp/v0.0.2 --force
 git tag transport/mqtt/v0.0.2 --force
+git tag transport/sse/v0.0.2 --force
 
 git tag bootstrap/v0.1.16 --force
 
